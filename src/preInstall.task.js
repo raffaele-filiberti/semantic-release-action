6207c56fd31273a0fa1312243fe6b49f1ac55a7a
raffaele-filiberti <raffaele.filiberti@gmail.com>
2021-07-19 12:57:15 +0200

    remove silent npm args

--- src/preInstall.task.js
+++ src/preInstall.task.js
@@ -13,7 +13,7 @@ module.exports = async extras => {
 
   const _extras = extras.replace(/['"]/g, '').replace(/[\n\r]/g, ' ');
 
-  const { stdout, stderr } = await exec(`npm install ${_extras} --silent`, {
+  const { stdout, stderr } = await exec(`npm install ${_extras}`, {
     cwd: path.resolve(__dirname, '..')
   });
   core.debug(stdout);
